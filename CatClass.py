ccc38a78d4319b786dbb8ea89ba5155a2e69b7f1
Lekosta <102583980+Levkovkosta@users.noreply.github.com>
2022-11-13 17:20:23 +0300

    Add files via upload

---
+++ CatClass.py
@@ -0,0 +1,12 @@
+class Cat:
+    def __init__(self, name, sex,breed, age,id_numb,status):
+        self.name=name
+        self.sex=sex
+        self.breed=breed
+        self.age=age
+        self.breed=breed
+        self.id_numb=id_numb
+        self.status=status
+    def getcard(self):
+        print(self.name,self.sex,self.breed,self.age,self.id_numb,self.status)
+
